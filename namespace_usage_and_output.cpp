4ff1148012bcfa8f4914cd5b0e38bbbaf90a98da
Princess-Treasure Chukwuemeka <theroyaloneseatsalone@gmail.com>
2026-02-28 10:00:39 +0200

    Add examples of namespace usage in C++

---
+++ namespace_usage_and_output.cpp
@@ -0,0 +1,27 @@
+METHOD ONE
+#include <iostream>
+using namespace std;
+int main() {
+        std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
+METHOD TWO 
+#include <iostream>
+using std::cout;
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+METHOD THREE
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
+ 
+
